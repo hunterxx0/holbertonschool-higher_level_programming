b7bbfce40ec597a286da6b21246b031647efb023
hunterxx0 <azouzimhamed@gmail.com>
2020-04-30 13:52:00 +0000

    first

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
-lis = dir(hidden_4)
-new = []
-for a in lis:
-    if a[0] != '_':
-        new.append(a)
-new.sort()
-for a in new:
-    print(a)
+if __name__ == "__main__":
+    import hidden_4
+    lis = dir(hidden_4)
+    new = []
+    for a in lis:
+        if a[0] != '_':
+            new.append(a)
+    new.sort()
+    for a in new:
+        print(a)
