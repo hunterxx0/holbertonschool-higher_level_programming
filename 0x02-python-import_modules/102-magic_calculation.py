69f5e74118ac2dc2681d9e3d4a34f105ed400ed4
hunterxx0 <azouzimhamed@gmail.com>
2020-04-30 15:19:57 +0000

    first

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b):
     from sys import argv, exit
     from magic_calculation_102 import add, sub
-    if b < a:
+    if b > a:
         c = add(b, a)
         for i in range(4, 6):
             c = add(i, c)
