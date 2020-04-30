55e209878f5a41528bb1d140cbfe41202138b0a3
hunterxx0 <azouzimhamed@gmail.com>
2020-04-30 15:17:42 +0000

    first

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from sys import argv, exit
+    from magic_calculation_102 import add, sub
+    if b < a:
+        c = add(b, a)
+        for i in range(4, 6):
+        add(i, c)
+        return c
+    else:
+        return sub(b, a)
