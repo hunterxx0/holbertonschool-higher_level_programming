f385e7c1af031480b7fa9b6b81c5aecf28d75e2f
hunterxx0 <azouzimhamed@gmail.com>
2020-05-05 16:30:30 +0000

    first

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,13 +1,29 @@
 #include "lists.h"
+/**
+ * lenls? (- len list)?
+ *
+ * @h: input head
+ * Return: l
+ */
+int lenls(listint_t **h)
+{
+	int ln = 0;
+	listint_t *t = *h;
 
+	while (t)
+	{
+		ln++;
+		t = t->next;
+	}
+	return (ln);
+}
 /**
  * revl? (- reverse list)?
  *
  * @h: input head
- * @z: input array
  * Return: n
  */
-int revl(listint_t **h, int *z)
+int *revl(listint_t **h, int *z)
 {
 	listint_t *t = *h;
 	int x = 0;
@@ -18,7 +34,7 @@ int revl(listint_t **h, int *z)
 		x++;
 		t = t->next;
 	}
-	return (x);
+	return (z);
 }
 /**
  * is_palindrome? (- palind)?
@@ -29,20 +45,24 @@ int revl(listint_t **h, int *z)
 int is_palindrome(listint_t **h)
 {
 	listint_t *t = *h;
-	int l = 0, i = 0, x, lis[100], r = 0;
+	int l = 0, i = 0, x, *lis,r = 0;
 
 	if (!t)
 		return (1);
-	l = revl(h, lis);
+	l = lenls(h);
+	lis = malloc(sizeof(int) * l);
+	if (!lis)
+		return (0);
+	lis = revl(h, lis);
 	x = l;
-	while (t && i < x)
+	while (i < x)
 	{
-		if (t->n != lis[l - 1])
+		if (lis[i] != lis[l - 1])
 			break;
 		i++;
-		t = t->next;
 		l--;
 	}
+	free(lis);
 	if (i == x)
 		r = 1;
 	return (r);
