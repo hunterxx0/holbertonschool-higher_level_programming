9b722cd4d4d388833506c3025ae55a9095634336
hunterxx0 <azouzimhamed@gmail.com>
2020-05-05 13:06:38 +0000

    first

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        y = len(matrix[0])
-        if y == 0:
-            print()
-            return
-        x = len(matrix)
-        i = 0
-
-        while i < x:
-            j = 0
-            while j < y:
-                if j != y - 1:
-                    print("{}".format(matrix[i][j]), end=' ')
-                else:
-                    print("{}".format(matrix[i][j]))
-                j += 1
-            i += 1
+    y = len(matrix[0])
+    if y == 0:
+        print()
+        return
+    x = len(matrix)
+    i = 0
+    while i < x:
+        j = 0
+        while j < y:
+            if j != y - 1:
+                print("{}".format(matrix[i][j]), end=' ')
+            else:
+                print("{}".format(matrix[i][j]))
+            j += 1
+        i += 1
