d2bf76fc1da6bdfc015637c50d866a0382ef1446
hunterxx0 <azouzimhamed@gmail.com>
2020-05-05 13:20:21 +0000

    first

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(m=[]):
-    x = m[:]
-    for i in m:
-        if m[i] % 2 == 0:
-            x[i] = True
-        else:
-            x[i] = False
-    return x
+    if m is not None:
+        x = m[:]
+        for i in m:
+            if m[i] % 2 == 0:
+                x[i] = True
+            else:
+                x[i] = False
+        return x
