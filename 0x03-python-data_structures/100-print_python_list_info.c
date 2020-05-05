ed5db808ba8066e18443109c87824e5a664710c3
hunterxx0 <azouzimhamed@gmail.com>
2020-05-05 22:05:39 +0000

    first

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info? (- reverse list)?
+ *
+ * @h: input head
+ * Return: n
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t i = 0, x = Py_SIZE(p);
+	PyObject *z;
+	printf("[*] Size of the Python List = %lu\n",x);
+	printf("[*] Allocated = %lu\n", x);
+	if (x != 0)
+	{
+		while (i < x)
+		{
+			z = PyList_GetItem(p, i);
+			printf("Element %lu: %s\n",i, Py_TYPE(z)->tp_name);
+			i++;
+		}
+	}
+}
