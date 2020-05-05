288b6031124bfff9d052f0fa1fba6a31abe0f80b
hunterxx0 <azouzimhamed@gmail.com>
2020-05-05 17:31:02 +0000

    first

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,18 +1,33 @@
 #include "lists.h"
+/**
+ * lenls? (- len list)?
+ *
+ * @h: input head
+ * Return: l
+ */
 int lenls(listint_t **h)
 {
 	int ln = 0;
 	listint_t *t = *h;
+
 	while (t)
 	{
-		ln++; t = t->next;
+		ln++;
+		t = t->next;
 	}
 	return (ln);
 }
+/**
+ * is_palindrome? (- palind)?
+ *
+ * @h: input head
+ * Return: 1 or 0
+ */
 int is_palindrome(listint_t **h)
 {
 	listint_t *t = *h;
 	int l = 0, i = 0, r = 0, s = 0;
+
 	if (!t || !t->next)
 		return (1);
 	l = lenls(h);
