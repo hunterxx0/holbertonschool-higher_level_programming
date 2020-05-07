fb15489a199449f1b15e5ebda2d15f6ea4fa9ef4
hunterxx0 <azouzimhamed@gmail.com>
2020-05-07 23:37:01 +0000

    first

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,31 +1,18 @@
 #include <Python.h>
 /**
- * print_python_list? (- reverse list)?
+ * print_python_bytes? (- reverse list)?
  *
- * @h: input head
- * Return: n
+ * @p: input head
+ * Return:
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t /*i = 0,*/ x = PyBytes_Size(p);
-/*	int m = 0;
-	if (*p)
-	{
-*/		printf("[.] bytes object info\n");
-		printf("  size: %lu\n",x);
-		printf("  trying string: %s\n", PyBytes_AsString(p));
-		/*len = first Number of bytes if <= 10;*/
-/*		for (;m <= 10; m++)
-			;
-		for (;i <= 10 || (unsigned int)p[i]; i++)
-		printf("  first %d bytes: %02x\n",m, (unsigned int)p[i]);
-	}
-	else
-	{
-		printf("  [ERROR] Invalid Bytes Object");
-	}
-*/
+	Py_ssize_t x = PyBytes_Size(p);
 
+	printf("[.] bytes object info\n");
+	printf("  size: %lu\n", x);
+	printf("  trying string: %s\n", PyBytes_AsString(p));
+	printf("  first %lu bytes: %02x\n", x, 000000);
 
 
 }
@@ -33,8 +20,8 @@ void print_python_bytes(PyObject *p)
 /**
  * print_python_list? (- reverse list)?
  *
- * @h: input head
- * Return: n
+ * @p: input head
+ * Return:
  */
 void print_python_list(PyObject *p)
 {
@@ -42,14 +29,14 @@ void print_python_list(PyObject *p)
 	PyObject *z;
 	PyListObject *ls = (PyListObject *)p;
 
-	printf("[*] Size of the Python List = %lu\n",x);
+	printf("[*] Size of the Python List = %lu\n", x);
 	printf("[*] Allocated = %lu\n", (unsigned long)(ls->allocated));
 	if (x != 0)
 	{
 		while (i < x)
 		{
 			z = PyList_GetItem(p, i);
-			printf("Element %lu: %s\n",i, Py_TYPE(z)->tp_name);
+			printf("Element %lu: %s\n", i, Py_TYPE(z)->tp_name);
 			i++;
 		}
 	}
