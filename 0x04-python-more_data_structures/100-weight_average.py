80686899743bf4f9a65e2c2345272b550455fdbe
hunterxx0 <azouzimhamed@gmail.com>
2020-05-07 18:20:56 +0000

    first

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-def mul(x, y):
-    if x is None:
-        x = 0
-    if y is None:
-        y = 0
-    return x * y
-
-
 def weight_average(m=[]):
     if m is None:
         return 0
     s, z = 0, 0
     for x, y in m:
-        a, b = x, y
-        if x is None:
-            a = 0
-        if y is None:
-            b = 0
-        s += mul(a, b)
-        z += b
+        s += x * y
+        z += y
     return s / z
