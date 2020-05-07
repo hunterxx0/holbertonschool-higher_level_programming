e0b4a2652fe1ce667d4641018fcbdd3e57ceb199
hunterxx0 <azouzimhamed@gmail.com>
2020-05-07 14:06:42 +0000

    first

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is not None:
-        m = []
-        i = 0
-        ls = []
-        for i in range(len(matrix[i])):
-            ls = [x**2 for x in matrix[i]]
-            m.append(ls)
-        return m
+    m = [[x ** 2 for x in ls] for ls in matrix]
+    return m
