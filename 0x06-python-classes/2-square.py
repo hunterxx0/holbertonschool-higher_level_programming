12224d57565a216399473a67226902dec72fb67f
hunterxx0 <azouzimhamed@gmail.com>
2020-05-19 17:07:26 +0000

    first

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class Square that manages: size
+"""
+
+
+class Square:
+    """a class Square with size management
+    """
+    def __init__(self, size=0):
+            if isinstance(size, int):
+                if size < 0:
+                    raise TypeError('size must be >= 0')
+                else:
+                    self.__size = size
+            else:
+                raise TypeError('size must be an integer')
