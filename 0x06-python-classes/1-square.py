2f4b84eb8b8e569eb9c8dc469f4b0493debe2668
hunterxx0 <azouzimhamed@gmail.com>
2020-05-19 16:18:20 +0000

    first

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class Square that manages: size
+"""
+
+
+class Square:
+    """a class Square with size management
+    """
+    def __init__(self, size):
+        self.__size = size
