3db6d8d10602d9222a84a3f7155d2d3c50c98d03
hunterxx0 <azouzimhamed@gmail.com>
2020-05-26 21:05:29 +0000

    first

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 
-def magic_string():
-    i = self.i
-    print(("holberton, " * (i)) + ("holberton" * (i - (i - 1))), end="\n")
+def magic_string(m=[-1]):
+    m[0] += 1
+    return ("holberton, " * (m[0]) + ("holberton" * (m[0] - (m[0] - 1))))
