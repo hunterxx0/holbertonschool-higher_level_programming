0c34f2ec4a6d0c6dc74c1283b0667b555c8884ff
hunterxx0 <azouzimhamed@gmail.com>
2020-06-03 21:44:36 +0000

    first

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,7 +5,18 @@ Writes a text in a file function: write_file()
 1 line
 """
 import sys
+import signal
 
+def sighand(signal, frame):
+    print("File size: {}".format(ss[8][:-1]))
+    ks = list(dt.keys())
+    ks.sort()
+    for x in ks:
+        if dt[x] != 0:
+            print("{}: {}".format(x, dt[x]))
+    raise KeyboardInterrupt
+
+signal.signal(signal.SIGINT, sighand)
 
 dt = {
     '200': 0,
@@ -16,18 +27,18 @@ dt = {
     '404': 0,
     '405': 0,
     '500': 0
-
 }
+
 c = 1
 for line in sys.stdin:
     ss = line.split(' ')
     if ss[7] in dt:
         dt[ss[7]] += 1
     if c != 0 and c % 10 == 0:
+        print("File size: {}".format(ss[8][:-1]))
         ks = list(dt.keys())
         ks.sort()
         for x in ks:
             if dt[x] != 0:
                 print("{}: {}".format(x, dt[x]))
-        print("File size: {}".format(ss[8][:-1]))
     c += 1
