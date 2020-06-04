dc53ee6a993becf8d7450484881c0103a8073e1a
hunterxx0 <azouzimhamed@gmail.com>
2020-06-04 10:31:27 +0000

    first

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -27,20 +27,31 @@ dt = {
     '405': 0,
     '500': 0
 }
+fs = 0
+nn = 0
 zz = 0
 c = 1
 for line in sys.stdin:
-    ss = line.split(' ')
-    if ss[7]:
-        if ss[7] in dt:
-            dt[ss[7]] += 1
-        if c != 0 and c % 10 == 0:
-            if ss[8] and ss[8] != "\n":
-                zz = ss[8][:-1]
-            print("File size: {}".format(zz))
-            ks = list(dt.keys())
-            ks.sort()
-            for x in ks:
-                if dt[x] != 0:
-                    print("{}: {}".format(x, dt[x]))
-    c += 1
+    if line:
+        ss = line.split(' ')
+        if ss[7]:
+            if ss[7] in dt:
+                dt[ss[7]] += 1
+            if c != 0 and c % 10 == 0:
+                if ss[8] and ss[8] != "\n":
+                    zz = ss[8][:-1]
+                    fs = zz
+                print("File size: {}".format(zz))
+                ks = list(dt.keys())
+                ks.sort()
+                for x in ks:
+                    if dt[x] != 0:
+                        print("{}: {}".format(x, dt[x]))
+        c += 1
+    else:
+        print("File size: {}".format(fs))
+        ks = list(dt.keys())
+        ks.sort()
+        for x in ks:
+            if dt[x] != 0:
+                print("{}: {}".format(x, dt[x]))
