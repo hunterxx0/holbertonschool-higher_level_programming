098d420a3d01fba3aae4b29222f98b3cba321726
hunterxx0 <azouzimhamed@gmail.com>
2020-06-09 15:56:10 +0000

    first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,6 @@ from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init funct"""
         super().__init__(id)
         self.width = width
         self.height = height
