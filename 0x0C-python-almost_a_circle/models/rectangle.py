e1b9bffdb6454ab53eb63cc90c2a6fd1f2921feb
hunterxx0 <azouzimhamed@gmail.com>
2020-06-09 13:22:30 +0000

    first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,6 @@ class Rectangle(Base):
     """
     Rectangle class
     """
-    __nb_objects = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
--- 0x0C-python-almost_a_circle/try.py
+++ 0x0C-python-almost_a_circle/try.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
-""" 100-main """
+""" 1-main """
 from models.rectangle import Rectangle
-from models.square import Square
 
 if __name__ == "__main__":
 
-    r1 = Rectangle(10, 7, 2, 8)
-    r2 = Rectangle(2, 4)
-    print(r1.x)
+    r1 = Rectangle(10, 2)
+    print(r1.id)
 
-    list_rectangles_input = [r1, r2]
+    r2 = Rectangle(2, 10)
+    print(r2.id)
 
-    Rectangle.save_to_file_csv(list_rectangles_input)
-
-    list_rectangles_output = Rectangle.load_from_file_csv()
-
-    for rect in list_rectangles_input:
-        print("[{}] {}".format(id(rect), rect))
-
-    print("---")
-
-    for rect in list_rectangles_output:
-        print("[{}] {}".format(id(rect), rect))
-
-    print("---")
-    print("---")
-
-    s1 = Square(5)
-    s2 = Square(7, 9, 1)
-    list_squares_input = [s1, s2]
-
-    Square.save_to_file_csv(list_squares_input)
-
-    list_squares_output = Square.load_from_file_csv()
-
-    for square in list_squares_input:
-        print("[{}] {}".format(id(square), square))
-
-    print("---")
-
-    for square in list_squares_output:
-        print("[{}] {}".format(id(square), square))
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
