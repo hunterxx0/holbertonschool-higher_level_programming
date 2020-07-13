bd229f00ca4979659a59ada826ee2189c46d3754
hunterxx0 <azouzimhamed@gmail.com>
2020-07-13 12:17:28 +0000

    first

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- deletes database in your MySQL server if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- creates a new database in your MySQL server if it doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0
