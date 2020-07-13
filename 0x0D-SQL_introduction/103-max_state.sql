cfd1d482ae49e3cbfa6cff27d15865d8a336ce3c
hunterxx0 <azouzimhamed@gmail.com>
2020-07-13 15:53:19 +0000

    first

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,5 @@
 -- displays the average temperature by city ordered by temperature
-SELECT state, MAX(value) as max_temp from temperatures
-GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp
+from temperatures
+GROUP BY state
+ORDER BY state;
