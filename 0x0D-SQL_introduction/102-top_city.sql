f35c361ce8ce455e685477209e1ced8a55ac9803
hunterxx0 <azouzimhamed@gmail.com>
2020-07-13 15:54:21 +0000

    first

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,6 @@
 -- displays the average temperature by city ordered by temperature
-SELECT city, AVG(value) as avg_temp from temperatures
-WHERE month BETWEEN 7 AND 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp
+from temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;
