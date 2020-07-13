4328fa0367e92681722d44500536bd975b38d65e
hunterxx0 <azouzimhamed@gmail.com>
2020-07-13 15:34:15 +0000

    first

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all rows of the table by desc order
-SELECT score,name FROM second_table ORDER BY score DESC;
+SELECT score,name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
