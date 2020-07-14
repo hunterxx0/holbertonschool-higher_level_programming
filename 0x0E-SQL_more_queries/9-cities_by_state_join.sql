119cab297294e7bd7c3ffc9226942a6adb4c9868
hunterxx0 <azouzimhamed@gmail.com>
2020-07-14 12:17:30 +0000

    first

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- using join
-SELECT cities.id cities.name, states.name FROM cities LEFT JOIN states
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states
 ON cities.state_id = states.id ORDER BY cities.id;
