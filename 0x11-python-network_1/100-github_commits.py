bc6bbbe0a39c2e692251884623108439527a518e
hunterxx0 <azouzimhamed@gmail.com>
2020-08-24 15:06:15 +0000

    first

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     c = 0
     for lines in res:
         print("{}: {}".format(lines.get('sha'),
-                             lines.get('commit').get('author').get('name')))
+                              lines.get('commit').get('author').get('name')))
         c += 1
         if c == 10:
             break
