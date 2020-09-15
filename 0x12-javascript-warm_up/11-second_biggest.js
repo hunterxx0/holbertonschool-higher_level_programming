e9e23be7289225142fa5444021ac0d70c884e4d1
hunterxx0 <azouzimhamed@gmail.com>
2020-09-15 20:07:07 +0000

    first

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,20 +2,8 @@
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
 } else {
-  let max = 0;
-  let sm = 0;
-  if (parseInt(process.argv[2]) > parseInt(process.argv[3])) {
-    max = parseInt(process.argv[2]);
-    sm = parseInt(process.argv[3]);
-  } else {
-    max = parseInt(process.argv[3]);
-    sm = parseInt(process.argv[2]);
-  }
-  for (let i = 4; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > max) {
-      sm = max;
-      max = process.argv[i];
-    }
-  }
-  console.log(sm);
+  const arr = process.argv.slice();
+  arr.shift();
+  arr.shift();
+  console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
 }
