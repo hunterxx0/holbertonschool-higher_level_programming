604d303169ab2a3c3b91659f4462044a8efcda44
hunterxx0 <azouzimhamed@gmail.com>
2020-09-15 19:45:09 +0000

    first

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log(1);
+  console.log(0);
 } else {
-  let max = process.argv[2];
+  let max = 0;
   let sm = 0;
-  for (let i = 3; i < process.argv.length; i++) {
+  if (parseInt(process.argv[2]) > parseInt(process.argv[3])) {
+    max = parseInt(process.argv[2]);
+    sm = parseInt(process.argv[3]);
+  } else {
+    max = parseInt(process.argv[3]);
+    sm = parseInt(process.argv[2]);
+  }
+  for (let i = 4; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > max) {
       sm = max;
       max = process.argv[i];
