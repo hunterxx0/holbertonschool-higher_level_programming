b28b27e73edaa9667801f7d95625e373353410ed
hunterxx0 <azouzimhamed@gmail.com>
2020-09-15 18:33:49 +0000

    first

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (isNaN(Number(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
