75dc9f2a928991b874df1711c1791dbf40b5a833
hunterxx0 <azouzimhamed@gmail.com>
2020-09-21 12:40:40 +0000

    first

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  if (list === undefined) {
+    return 0;
+  }
   const count = {};
   list.forEach(function (x) { count[x] = (count[x] || 0) + 1; });
   if (count[searchElement] === undefined) {
