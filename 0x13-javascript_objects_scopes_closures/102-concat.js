4e54cd8360af68c85c8ffce7bc60e333b32ddf90
hunterxx0 <azouzimhamed@gmail.com>
2020-09-21 16:24:36 +0000

    first

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const fs = require('fs');
-let data = ''
-fs.readFile(args[0], 'utf8', function (err,data1) {
-  fs.readFile(args[1], 'utf8', function (err,data2) {
-    if (err) {
-      return console.log(err);
+let data = '';
+fs.readFile(args[0], 'utf8', function (err1, data1) {
+  fs.readFile(args[1], 'utf8', function (err2, data2) {
+    if (err1 || err2) {
+      return console.log(err1 || err2);
     }
     data = data1 + data2;
     fs.writeFile(args[2], data, { flag: 'wx' }, function (err) {
