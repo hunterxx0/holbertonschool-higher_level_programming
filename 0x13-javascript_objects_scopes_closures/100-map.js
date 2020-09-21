f53eb9f24d4ac16420e9e1cbd8b2e1d2b9a2e75c
hunterxx0 <azouzimhamed@gmail.com>
2020-09-21 14:37:56 +0000

    first

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-const list1 = list.map(x => (x * Number(list.indexOf(x))));
-console.log(list1)
+const list1 = list.map((x, y) => x * y);
+console.log(list1);
