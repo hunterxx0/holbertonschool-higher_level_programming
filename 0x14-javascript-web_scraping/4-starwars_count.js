2bd3120b8df8f253add6566f4edb175e5d522da0
hunterxx0 <azouzimhamed@gmail.com>
2020-09-22 12:46:58 +0000

    first

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const request = require('request');
-let count = 0;
 request(args[0], function (error, response, body) {
   if (error) {
     return console.log(error);
   }
+  let count = 0;
   const res = JSON.parse(body).results;
   for (const x in res) {
     for (const charac in res[x].characters) {
-      if (res[x].characters[charac].slice(37, -1) === '18') {
+      if (res[x].characters[charac] &&
+          res[x].characters[charac].slice(37, -1) === '18') {
         count++;
       }
     }
