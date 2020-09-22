a50f05295f372901aa40bc57071b9783c42bace9
hunterxx0 <azouzimhamed@gmail.com>
2020-09-22 12:42:16 +0000

    first

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const request = require('request');
-request(args[0], function (error, response, body) {
+const url = "https://swapi-api.hbtn.io/api/films/" + args[0];
+request(url, function (error, response, body) {
   if (error) {
     return console.log(error);
   }
-  console.log('code: ' + response.statusCode);
+  console.log(JSON.parse(body).title);
 });
